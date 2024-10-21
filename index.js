19258bb6b2ce1ddb3e88dd17d2283df38aaab117
Trostnik06 <trostnikov06@mail.ru>
2024-10-21 11:08:05 +0000

    aaaa

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/hello', (req, res) => {
+  const name = req.query.name || 'World';
+
+  res.send(`Hello, ${name}!`);
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
