2b75e656cb8fe4ef6c360d0096cbbe9172b02386
Trostnik06 <trostnikov06@mail.ru>
2024-10-21 13:24:17 +0000

    Project_add

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
+const port = 3000;
 
-app.get('/hello', (req, res) => {
-  const name = req.query.name || 'World';
-  res.send(`Greetings, ${name}!`);
+const mongoURL = process.env.MONGO_URL || 'mongodb://localhost:27017/mydatabase';
+mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
 });
